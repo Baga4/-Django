c36001ad45413ab01dc25afd2a01ec5aa784e1ed
SagarDonut <fighterworld667@gmail.com>
2024-06-23 15:15:27 +0545

    added delete func

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('logout', views.user_logout , name='logout'),
     path('signin', views.user_signin , name='signin'),
     path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.forms import UserCreationForm , AuthenticationForm 
 from django.contrib.auth import authenticate,login,decorators
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import post
 from . import forms 
 import datetime                                                                                      
@@ -9,23 +10,20 @@ import datetime
 
 @login_required
 def home(request):
-    user=request.user
-    if user.is_authenticated: 
-        content = post.objects.all().order_by("-created_at")
-    else:
-        content=None
-   
+    user=request.user 
+    contents = post.objects.all().order_by("-created_at")
+    
     if request.method == 'POST':
         auther=user.username
         title=request.POST['title']
         dis=request.POST['description']
         new_user=post( auther=auther,title=title,description=dis)
         new_user.save()
-        print('new post added')
+        messages.SUCCESS(request," sucessfully posted")
         return redirect('home')
 
-    return render(request,'index.html',{
-        'content':content,
+    return render(request,'index.html', {
+        'contents':contents,
         'user':user,
         'forms':forms,
         })
@@ -56,11 +54,14 @@ def user_signin(request):
     else:
         form=UserCreationForm() 
     return render(request,'signin.html', { 'form':form})
- 
+
+def delete_post(request ,post_id):
+    Post_instance = get_object_or_404 (post ,id=post_id)
+    Post_instance.delete()
+    return redirect("home")
+
 def edit_post(request, post_id):
-    print(post_id)
     Post_instance = get_object_or_404(post ,id=post_id)
-    print(Post_instance)
     if request.method == "POST":
         form = forms.postForm(request.POST ,instance=Post_instance)
         if form.is_valid():  
