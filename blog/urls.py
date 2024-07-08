59498c1181878fae9abc22ed09b5cde28814e2b6
SagarDonut <fighterworld667@gmail.com>
2024-07-08 16:30:02 +0545

    fixed index

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,9 @@ urlpatterns = [
     path('', views.home , name='home'),
     path('login', views.user_login , name='login'),
     path('accounts/login/', views.user_login , name='login'),
-    path('logout', views.user_logout , name='logout'),
+    path('accounts/signin/', views.user_login , name='signin'),
     path('signin', views.user_signin , name='signin'),
+    path('logout', views.user_logout , name='logout'),
     path('edit/<int:post_id>/', views.edit_post, name='edit_post'),
     path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
 
