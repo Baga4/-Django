887faf649d8a139bcc5fdd927305ee048b934492
SagarDonut <fighterworld667@gmail.com>
2024-08-31 10:12:57 +0545

    integrated form validations

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,15 @@ class postForm(forms.ModelForm):
     class Meta:
         model = post
         fields = ['title', 'description' ,]
+    #added forms validation
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) < 5:
+            raise forms.ValidationError('Title must be at least 5 characters long.')
+        return title
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        if len(description) < 10:
+            raise forms.ValidationError('Description must be at least 10 characters long.')
+        return description
